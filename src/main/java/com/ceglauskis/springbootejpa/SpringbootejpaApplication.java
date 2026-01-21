982fdfddf2cf103422d073b337c1f5b2c93bb3d9
maikoceglauskis@gmail.com <maikoceglauskis@gmail.com>
2026-01-21 10:17:43 -0300

    Initial commit - Spring Boot JPA project

---
+++ src/main/java/com/ceglauskis/springbootejpa/SpringbootejpaApplication.java
@@ -0,0 +1,13 @@
+package com.ceglauskis.springbootejpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootejpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootejpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ceglauskis/springbootejpa/SpringbootejpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceglauskis.springbootejpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootejpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
