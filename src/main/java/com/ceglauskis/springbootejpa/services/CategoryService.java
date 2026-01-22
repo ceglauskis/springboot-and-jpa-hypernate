cea82716314516162e863cc51fb4e6205ddaed31
maikoceglauskis@gmail.com <maikoceglauskis@gmail.com>
2026-01-22 17:40:14 -0300

    Category implementation

---
+++ src/main/java/com/ceglauskis/springbootejpa/services/CategoryService.java
@@ -0,0 +1,25 @@
+package com.ceglauskis.springbootejpa.services;
+
+import com.ceglauskis.springbootejpa.entities.Category;
+import com.ceglauskis.springbootejpa.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    public List<Category> findAll(){
+        return repository.findAll();
+    }
+
+    public Category findById(Long id){
+        Optional<Category> obj = repository.findById(id);
+        return obj.get();
+    }
+}
